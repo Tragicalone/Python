f69b0f0a70271d7beced9f80b337ba9a3cb5b865
Tragicalone <tragicalone@yahoo.com.tw>
2018-08-21 15:17:57 +0800

    Add Start.py

---
+++ Start.py
@@ -0,0 +1,18 @@
+# Comment
+data1 = print("Hello Python")
+print(data1)
+
+data2 = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+print(data2)
+data2 = list(input("Insert Number"))
+print(data2)
+if (1, 2, 3)in data2:
+    print(101 // 10)
+    print(10 // 3.5)
+else:
+    print(10 % 3.5)
+    print(10 ** 8)
+print("""Hello
+world
+I am Python""")
+print("Hello World"[6])
