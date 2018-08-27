9d2f878328b20a55440bace56eaf34fad6261945
Tragicalone <tragicalone@yahoo.com.tw>
2018-08-27 14:33:15 +0800

    findAll 修復

--- Start.py
+++ Start.py
@@ -13,13 +13,6 @@ else:
         Soup = BeautifulSoup(Response.text, "lxml")
         FileError.write(Soup.find("h1").text)
         FileError.write('\n' + '_' * 50 + '\n')
-        FileError.write(str(Soup.h1))
-        FileError.write('\n' + '_' * 50 + '\n')
-        FileError.write(str(Soup.html.h1))
-        FileError.write('\n' + '_' * 50 + '\n')
-        FileError.write(str(Soup.body.h1))
-        FileError.write('\n' + '_' * 50 + '\n')
-        FileError.write(str(Soup.html.body.h1))
 print("使用 ", (datetime.datetime.now() - StartTime).total_seconds(), " 秒")
 
 from selenium import webdriver
@@ -28,30 +21,22 @@ ChromeDriver = webdriver.Chrome("D:/Document/Python/chromedriver")
 ChromeDriver.get("http://www.sharecourse.net/sharecourse/course")
 Soap = BeautifulSoup(ChromeDriver.page_source, "lxml")
 with open("Error.log", mode="a", encoding='UTF-8') as FileError:
-    ClassDivs = Soap.findAll("div", {"class": "card hvr-underline-from-center"})
-    for TheDiv in ClassDivs:
+    for ClassDiv in Soap.findAll("div", {"class": "card hvr-underline-from-center"}):
         FileError.write('\n' + '_' * 50 + '\n')
-        Temp3 = TheDiv.contents.findall("div", {"class": "card-body"})
-        Temp1 = TheDiv.get("onclick")
-        Temp2 = TheDiv.get("onclick").text
-        # FileError.write(TheDiv.get("onclick").text + '\n')
+        for BodyDiv in ClassDiv.findAll("div", {"class": "card-body"}):
+            FileError.write(BodyDiv.text + '\n')
+        FileError.write(ClassDiv.get("onclick") + '\n')
 ChromeDriver.close()
 print("使用 ", (datetime.datetime.now() - StartTime).total_seconds(), " 秒")
-
-# data1 = print("Hello Python")
-# print(data1)
 
 # data2 = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
 # print(data2)
 # data2 = list(input("Insert Number: "))
 # print(data2)
-# if (1, 2, 3)in data2:
+# if (1, 2, 3) in data2:
 #     print(101 // 10)
 #     print(10 // 3.5)
 # else:
 #     print(10 % 3.5)
 #     print(10 ** 8)
-# print("""Hello
-# world
-# I am Python""")
-# print("Hello World"[6])
+# print("Hello\nworld\nI am Python")
